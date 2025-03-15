82aea01ec6c4ab7f1d6540a5a12e2f2cda52ebe6
CaiqueGabriel <worldcadiiin@gmail.com>
2025-03-15 19:18:22 -0300

    commitando aula 8

---
+++ cursophp/aulas/aula_8.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Minha aula de PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            $saudacao = "Olá,";
+            $localidade = "Mundo!";
+            $titulo = 1;
+            echo "$saudacao virgula $localidade";
+            echo '$saudacao $localidade';
+        ?>
+    </h1>
+</body>
+</html>
