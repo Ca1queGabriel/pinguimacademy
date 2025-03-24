c7716a19a2e8fbe91e4149eefe2a121bdc1f6a1a
CaiqueGabriel <worldcadiiin@gmail.com>
2025-03-23 21:37:27 -0300

    Atualização do 'require.php'

--- cursophp/aulas/aula_12_IncludeRequire/include.php
+++ cursophp/aulas/aula_12_IncludeRequire/include.php
@@ -15,25 +15,32 @@ declare(strict_types = 1);
 <head>
     <?php include 'inc/head.php'; ?>
     <?php $config = include ('inc/config.php');?>
+    <?php include_once('inc/greetings.php'); ?>
     <title>Include</title>
 </head>
 <body>
 
     <h1><?= 'Include' ?></h1>
+
+    <div>
         <table>
             <thead>
-                <tr>
-                    <th>Usuário</th>
-                    <th>Senha</th>
-                </tr>
+            <tr>
+                <th>Usuário</th>
+                <th>Senha</th>
+            </tr>
             </thead>
-                <tbody>
-                <tr>
-                    <td><?= $config['username'] ?></td>
-                    <td><?= $config['password'] ?></td>
-                </tr>
-                </tbody>
+            <tbody>
+            <tr>
+                <td><?= $config['username'] ?></td>
+                <td><?= $config['password'] ?></td>
+            </tr>
+            </tbody>
         </table>
+    </div>
 
+    <div style="margin-top: 50px">
+    <h2>Olá <?= $nome ?> </h2>
+    </div>
 </body>
 </html>
