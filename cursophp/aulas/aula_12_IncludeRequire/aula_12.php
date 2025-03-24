720cb15dd2e6af358567ea74763071d4d615fba7
CaiqueGabriel <worldcadiiin@gmail.com>
2025-03-23 21:05:26 -0300

    Criação do arquivo 'head.php'

---
+++ cursophp/aulas/aula_12_IncludeRequire/aula_12.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Include e Require</title>
+</head>
+<body>
+    <h1>Include e Require</h1>
+    <ul>
+        <li>
+            <a href="/include.php">Include</a>
+        </li>
+        <li>
+            <a href="/require.php">Require</a>
+        </li>
+    </ul>
+</body>
+</html>
---
+++ cursophp/aulas/aula_12_IncludeRequire/include.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ *  Include
+ *
+ * Lança um warning
+ *
+ * é utilizado para incluir arquivos necessários no projeto
+ */
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <?php include 'head.php'; ?>
+    <title>Include</title>
+</head>
+<body>
+    <h1><?= 'Include' ?></h1>
+</body>
+</html>
