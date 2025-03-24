92562702e3fc66dfbd8708dd32695ef50361d008
CaiqueGabriel <worldcadiiin@gmail.com>
2025-03-24 17:14:22 -0300

    Adiciona a tela de exemplo-basico

---
+++ cursophp/aulas/aula_13_funcoes/exemplo-basico.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    <h1>Exemplo Básico</h1>
+
+    <?php
+        function soma($a, $b)
+        {
+            return $a + $b;
+        }
+        echo soma(2,2);
+    ?>
+</body>
+</html>
---
+++ cursophp/aulas/aula_13_funcoes/exemplofuncao.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+/*
+ * Função
+ *
+ *
+ */
+function soma($a, $b){
+    return $a + $b;
+}
+
+
+$resultado = soma(1,2);
+echo $resultado;
