2d66c5de693e3b911e1bccd97d1ced1c0b1565b8
CaiqueGabriel <worldcadiiin@gmail.com>
2025-03-23 21:14:37 -0300

    Adicionado o arquivo 'config.php' na pasta 'inc'

--- cursophp/aulas/aula_12_IncludeRequire/include.php
+++ cursophp/aulas/aula_12_IncludeRequire/include.php
@@ -13,10 +13,27 @@ declare(strict_types = 1);
 <!doctype html>
 <html lang="en">
 <head>
-    <?php include 'head.php'; ?>
+    <?php include 'inc/head.php'; ?>
+    <?php $config = include ('inc/config.php');?>
     <title>Include</title>
 </head>
 <body>
+
     <h1><?= 'Include' ?></h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Usuário</th>
+                    <th>Senha</th>
+                </tr>
+            </thead>
+                <tbody>
+                <tr>
+                    <td><?= $config['username'] ?></td>
+                    <td><?= $config['password'] ?></td>
+                </tr>
+                </tbody>
+        </table>
+
 </body>
 </html>
